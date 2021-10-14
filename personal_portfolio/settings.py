dfa9863f4e3473e0274424dbd072e63bef495d12
Panos <panagiotiskas@hotmail.com>
2021-10-14 16:45:40 +0300

    update

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-ll%mw^t3#05fsk)(*sbrj5lkhyoaq=b@#r6=pl6)a3&3r=t^f#'
+SECRET_KEY = 'django-insecure-ll%mw^t3#05fsk)(*sbrj5lkhyoaq=b@#r6=pl6)a3&3r=t^f#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
